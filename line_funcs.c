2edff05aa665739e83c8ded54c52f69eb42eec66
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-27 12:51:29 +0000

    betty correction

--- line_funcs.c
+++ line_funcs.c
@@ -6,27 +6,27 @@
  */
 char *read_line(void)
 {
-  char *buf = NULL; /* holds line contents */
-  size_t buf_size = 0;
-  ssize_t line_size;
+char *buf = NULL; /* holds line contents */
+size_t buf_size = 0;
+ssize_t line_size;
 
-  /* Get the first line of the file. */
-  line_size = getline(&buf, &buf_size, stdin);
+/* Get the first line of the file. */
+line_size = getline(&buf, &buf_size, stdin);
 
-  if (line_size == EOF)
-    {
-      free(buf);
-      exit(EXIT_SUCCESS);
-    }
+if (line_size == EOF)
+{
+free(buf);
+exit(EXIT_SUCCESS);
+}
 
-  fflush(stdin);
+fflush(stdin);
 
-  /* Free the allocated line buffer */
-  free(buf);
-  /* changing end of line to null terminating */
-  buf = NULL;
+/* Free the allocated line buffer */
+free(buf);
+/* changing end of line to null terminating */
+buf = NULL;
 
-  return (buf);
+return (buf);
 }
 
 /**
@@ -36,20 +36,20 @@ char *read_line(void)
  */
 char **tokenize_line(char *arguments)
 {
-  size_t num_toks;
-  int index = 0;
-  char **cmd_line_toks = malloc(sizeof(char *) * num_toks);
-  char *cmd_line;
+size_t num_toks;
+int index = 0;
+char **cmd_line_toks = malloc(sizeof(char *) * num_toks);
+char *cmd_line;
 
-  cmd_line = strtok(arguments, " ");
-  if (cmd_line != NULL)
-    {
-      cmd_line_toks[index] = cmd_line;
-      index++;
-      cmd_line = strtok(NULL, " ");
-    }
-  cmd_line_toks[index] = NULL;
-  free(cmd_line_toks);
+cmd_line = strtok(arguments, " ");
+if (cmd_line != NULL)
+{
+cmd_line_toks[index] = cmd_line;
+index++;
+cmd_line = strtok(NULL, " ");
+}
+cmd_line_toks[index] = NULL;
+free(cmd_line_toks);
 
-  return (cmd_line_toks);
+return (cmd_line_toks);
 }
