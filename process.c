a46546ef8b594a55dada62342d7cd2437bee4660
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-27 19:42:51 +0000

    new path handling

--- process.c
+++ process.c
@@ -1,35 +1,58 @@
 #include "shell.h"
 
 /**
- * _process - prints PATH when commands/arguments are entered
- * @args: points to an array of pointers to strings
- * Return: 0 if successful
+ * path - searches directories in PATH for command
+ * @command: to search for
+ * @fullpath: full path of command to execute
+ * @path: full PATH variable
+ * Return: pointer to full_path
  */
-int _process(char **args)
+char *path(char *command, char *fullpath, char *path)
 {
+  unsigned int cmd_len, pth_len, orig_pth_len;
+  char *pth_cpy, *token;
 
-int pid;
-int child;
-int status;
+  cmd_len = _strlen(command);
+  orig_pth_len = _strlen(path);
+  pth_cpy = malloc(sizeof(char) * orig_pth_len + 1);
+  if (pth_cpy == NULL)
+    {
+      errors(3);
+      return (NULL);
+    }
+  _strcpy(pth_cpy, path);
+  /* copy PATH directory + command name and check if it exists */
+  
+  token = strtok(pth_cpy, ":"); /*tokenizes path to help with search*/
+  if (token == NULL)
+    token = strtok(NULL, ":");
+  while (token != NULL)
+    {
+      pth_len = _strlen(token);
+      fullpath = malloc(sizeof(char) * (pth_len + cmd_len) + 2);
+      if (fullpath == NULL)
+	{
+	  errors(3);
+	  return (NULL);
+	}
 
-child = fork();
+      
+      _strcpy(fullpath, token);
+      fullpath[pth_len] = '/';
+      _strcpy(fullpath + pth_len + 1, command);
+      fullpath[pth_len + cmd_len + 1] = '\0';
+      
+      /*test whether file exists and grants read permision*/
 
-if (child == 0)
-{
-if (execvp(args[0], args) < 0)
-{
-write(1, "comand not found: %s\n", 17);
-exit(EXIT_FAILURE);
-}
-else if (child < 0)
-writef(1, "error forking\n", 14);
-
-sleep(3);
-}
-else
-{
-wait(&status);
-}
-return (1);
-
+if (access(fullpath, X_OK) != 0)
+	{
+	  free(fullpath);
+	  fullpath = NULL;
+	  token = strtok(NULL, ":");
+	}
+      else
+	break;
+    }
+  free(pth_cpy);
+  return (fullpath);
 }
