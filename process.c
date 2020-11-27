5348e4601566cd46a47cdca1d4f0f09cda5835ce
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-27 10:30:29 +0000

    printf changes

--- process.c
+++ process.c
@@ -5,31 +5,31 @@
  * @args: points to an array of pointers to strings
  * Return: 0 if successful
  */
-int _process(char **args)                                        
-{                                                             
-                                                              
-  int pid;                                                  
-  int child;                                                
-  int status;                                               
-                                                              
-  child = fork();                                           
-                                                              
-  if (child == 0)                                           
-    {                                                       
-      if (execvp(args [0], args) < 0)                       
-	{                                                   
-	  printf("comand not found: %s\n", args[0]);        
-	  exit(EXIT_FAILURE);                               
-	}                                                   
-      else if (child < 0)                                   
-	printf("error forking\n");                          
-                                                              
-      sleep (3);                                            
-    }                                                       
-  else                                                      
-    {                                                       
-      wait(&status);                                        
-    }                                                       
-  return (1);                                               
-                                                              
-} 
+int _process(char **args)
+{
+
+int pid;
+int child;
+int status;
+
+child = fork();
+
+if (child == 0)
+{
+if (execvp(args[0], args) < 0)
+{
+write(1, "comand not found: %s\n", 17);
+exit(EXIT_FAILURE);
+}
+else if (child < 0)
+writef(1, "error forking\n", 14);
+
+sleep(3);
+}
+else
+{
+wait(&status);
+}
+return (1);
+
+}
