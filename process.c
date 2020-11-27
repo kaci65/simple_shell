6c2d8e2d03acc9ff3b6be56bf7bdc23456fa5355
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-11-27 09:35:04 +0000

    changed code

--- process.c
+++ process.c
@@ -5,26 +5,31 @@
  * @args: points to an array of pointers to strings
  * Return: 0 if successful
  */
-int _process(char **args)
-{
-	pid_t child;
-	int status;
-
-	if (child == 0)
-	{
-		if (execve(args[0], args, environ) < 0)
-		{
-			f_putstr("command not found: %s\n", args[0]);
-			exit(EXIT_FAILURE);
-		}
-		else if (child < 0)
-		{
-			f_putstr("fork error\n");
-			sleep(3);
-		}
-		else
-			wait(&status);
-
-		return (0);
-	}
-}
+int _process(char **args)                                        
+{                                                             
+                                                              
+  int pid;                                                  
+  int child;                                                
+  int status;                                               
+                                                              
+  child = fork();                                           
+                                                              
+  if (child == 0)                                           
+    {                                                       
+      if (execvp(args [0], args) < 0)                       
+	{                                                   
+	  printf("comand not found: %s\n", args[0]);        
+	  exit(EXIT_FAILURE);                               
+	}                                                   
+      else if (child < 0)                                   
+	printf("error forking\n");                          
+                                                              
+      sleep (3);                                            
+    }                                                       
+  else                                                      
+    {                                                       
+      wait(&status);                                        
+    }                                                       
+  return (1);                                               
+                                                              
+} 
