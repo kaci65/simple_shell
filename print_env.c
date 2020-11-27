0b958c1c6dd303a538105a56081efad389e6afb2
kaci65 <wanjikukarugi@gmail.com>
2020-11-27 20:23:10 +0300

    Edit file

--- print_env.c
+++ print_env.c
@@ -5,14 +5,13 @@
  * @environ: variable pointing to an array of pointers to strings called the "environment"
  * Return: 0 if successful
  */
-int print_env (char **environ)
+void print_env (char **environ)
 {
     int j;
 
     for (j = 0; environ[j]; j++)
     {
         write(STDOUT_FILENO, environ[j], _strlen(environ[j]));
-        f_putchar("\n");
+        f_putchar('\n');
     }
-    return (0);
 }
